716124a7bd3d547e7892518bf2590449230a283d
Sasha15 NA <sashau087@gmail.com>
2019-03-28 10:09:55 +0200

    b

--- bookingshelf-admin/src/EmailPage/EmailPage.jsx
+++ bookingshelf-admin/src/EmailPage/EmailPage.jsx
@@ -33,9 +33,7 @@ class EmailPage extends Component {
         ){
             props.history.push('/nopage')
         }
-
-        console.log(props.match.params);
-
+        
         if(!props.match.params.activeTab || props.match.params.activeTab==='auto_notification') {document.title = "Авто уведомления | Онлайн-запись";}
         if(props.match.params.activeTab==='smsletter'){document.title = "SMS Рассылка | Онлайн-запись"}
         if(props.match.params.activeTab==='newsletter'){document.title = "Email Рассылка | Онлайн-запись"}
