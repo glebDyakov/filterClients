6b62d31d2177ea189a42d31f86821819905b3489
vasylevych1998 <vasylevych1998@gmail.com>
2019-04-22 22:05:55 +0300

    removed disabled days

--- bookingshelf-online/src/IndexPage/IndexPage.jsx
+++ bookingshelf-online/src/IndexPage/IndexPage.jsx
@@ -129,12 +129,14 @@ class IndexPage extends React.Component {
         }
         const {company} = this.props.match.params
 
-        let daySelected = modifiers.selected ? undefined : moment(day);
+        console.log('day', day)
+        console.log('modifiers', modifiers)
+
+        let daySelected = !modifiers.selected && moment(day);
 
 
         this.setState({
-            ...this.state,
-            selectedDay: daySelected.toDate(),
+            selectedDay: daySelected ? daySelected.toDate() : this.state.selectedDay,
             screen: 4
         })
     }
