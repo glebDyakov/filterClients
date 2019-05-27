c66c7599b34021dc18541258d9f097c989e1a956
Mike.Gusev <mike.gusev@strings.com>
2019-05-27 12:56:24 +0300

    fix for refactoring

--- bookingshelf-admin/src/CalendarPage/components/TabScrollContent.jsx
+++ bookingshelf-admin/src/CalendarPage/components/TabScrollContent.jsx
@@ -1,16 +1,11 @@
 import React, {PureComponent} from 'react';
 import moment from 'moment';
-import TabScrollLeftMenu from "./TabScrollLeftMenu";
-import AppointmentCell from "./AppointmentCell";
-import ReservedCell from "./ReservedCell";
-import DefaultCell from "./DefaultCell";
+import TabScrollLeftMenu from './TabScrollLeftMenu';
 
 class TabScroll extends PureComponent{
 
     render(){
-        const {numbers, availableTimetable,selectedDays, closedDates, client, appointments,reservedTime,handleUpdateClient, approveAppointmentSetter,updateReservedId,changeTime } = this.props;
-
-        const sortedAvailableTimetable = availableTimetable && availableTimetable.sort((a, b) => a.firstName.localeCompare(b.firstName));
+        const {numbers, availableTimetable,selectedDays, closedDates, clients, appointments,reservedTime: reservedTimeFromProps ,handleUpdateClient, approveAppointmentSetter,updateReservedId,changeTime } = this.props;
 
         return(
             <div className="tabs-scroll"
@@ -19,7 +14,7 @@ class TabScroll extends PureComponent{
                 {numbers && numbers.map((time, key) =>
                     <div className="tab-content-list" key={key}>
                         <TabScrollLeftMenu time={time}/>
-                        {sortedAvailableTimetable && selectedDays.map((day) => sortedAvailableTimetable.map((workingStaffElement, staffKey) => {
+                        {availableTimetable && selectedDays.map((day) => availableTimetable.sort((a, b) => a.firstName.localeCompare(b.firstName)).map((workingStaffElement, staffKey) => {
                             let currentTime= parseInt(moment(moment(day).format('DD/MM')+' '+moment(time, 'x').format('HH:mm'), 'DD/MM HH:mm').format('x'));
                             let appointment = appointments &&
                                 appointments.map((appointmentStaff) =>
@@ -31,13 +26,13 @@ class TabScroll extends PureComponent{
                                     })
                                 );
 
-                            let reservedTime = reservedTime &&
-                                reservedTime.map((reserve) =>
+                            let reservedTime = reservedTimeFromProps &&
+                                reservedTimeFromProps.map((reserve) =>
                                     reserve.reservedTimes &&
                                     reserve.staff.staffId === workingStaffElement.staffId &&
-                                    reserve.reservedTimes.filter((reservedTime)=>{
-                                        return currentTime <= parseInt(reservedTime.startTimeMillis)
-                                            && parseInt(moment(moment(day).format('DD/MM')+' '+moment(numbers[key + 1], 'x').format('HH:mm'), 'DD/MM HH:mm').format('x')) > parseInt(reservedTime.startTimeMillis)
+                                    reserve.reservedTimes.filter((localReservedTime)=>{
+                                        return currentTime <= parseInt(localReservedTime.startTimeMillis)
+                                            && parseInt(moment(moment(day).format('DD/MM')+' '+moment(numbers[key + 1], 'x').format('HH:mm'), 'DD/MM HH:mm').format('x')) > parseInt(localReservedTime.startTimeMillis)
                                     })
                                 );
 
@@ -62,39 +57,161 @@ class TabScroll extends PureComponent{
                                     ));
                             let resultMarkup;
                             if(appointment && appointment[0] && appointment[0].length > 0) {
+                                let totalDuration = appointment[0][0].duration;
+                                let appointmentServices = [];
+                                let totalCount = 0;
+                                appointmentServices.push(appointment[0][0].serviceName);
+                                if (appointment[0][0].hasCoAppointments) {
+                                    appointments.forEach(staffAppointment => staffAppointment.appointments.forEach(currentAppointment => {
+                                        if (currentAppointment.coAppointmentId === appointment[0][0].appointmentId) {
+                                            totalDuration += currentAppointment.duration;
+                                            appointmentServices.push(currentAppointment.serviceName)
+                                            totalCount++;
+                                        }
+                                    }))
+                                }
+                                let extraServiceText;
+                                switch (totalCount) {
+                                    case 0:
+                                        extraServiceText = '';
+                                        break;
+                                    case 1:
+                                        extraServiceText = 'и ещё 1 услуга';
+                                        break;
+                                    case 2:
+                                    case 3:
+                                    case 4:
+                                        extraServiceText = `и ещё ${totalCount} услуги`;
+                                        break;
+                                    default:
+                                        extraServiceText = `и ещё 5+ услуг`;
+                                }
+                                const resultTextArea = `${appointment[0][0].serviceName} ${extraServiceText}`;
+                                resultMarkup = (
+                                    <div
+                                        className={currentTime <= moment().format("x")
+                                        && currentTime >= moment().subtract(15, "minutes").format("x") ? 'present-time ' : ''}
+                                    >
+                                        {!appointment[0][0].coAppointmentId && (
+                                            <div
+                                                className={"notes " + appointment[0][0].appointmentId + " " + appointment[0][0].color.toLowerCase() + "-color " + (parseInt(moment(currentTime).format("H")) >= 20 && 'notes-bottom')}
+                                                key={appointment[0][0].appointmentId + "_" + key}
+                                                id={appointment[0][0].appointmentId + "_" + workingStaffElement.staffId + "_" + appointment[0][0].duration + "_" + appointment[0][0].appointmentTimeMillis + "_" + moment(appointment[0][0].appointmentTimeMillis, 'x').add(appointment[0][0].duration, 'seconds').format('x')}
+                                            >
+                                                <p className="notes-title">
+                                                    {!appointment[0][0].online &&
+                                                    <span className="pen"
+                                                          title="Запись через журнал"/>}
+                                                    {/*<span className="men"*/}
+                                                    {/*title="Постоянный клиент"/>*/}
+                                                    {appointment[0][0].online &&
+                                                    <span className="globus"
+                                                          title="Онлайн-запись"/>}
+
+                                                    <span className="delete"
+                                                          data-toggle="modal"
+                                                          data-target=".delete-notes-modal"
+                                                          title="Отменить встречу"
+                                                          onClick={() => approveAppointmentSetter(appointment[0][0].appointmentId)}/>
+                                                    {appointment[0][0].hasCoAppointments && <span className="super-visit" title="Мультивизит"/>}
+                                                    <span className="service_time">
+                                                                                    {moment(appointment[0][0].appointmentTimeMillis, 'x').format('HH:mm')} -
+                                                        {moment(appointment[0][0].appointmentTimeMillis, 'x').add(totalDuration, 'seconds').format('HH:mm')}
+                                                                                </span>
+                                                </p>
+                                                <p className="notes-container"
+                                                   style={{height: ((totalDuration / 60 / 15) - 1) * 20 + "px"}}>
+                                                    <textarea disabled>{resultTextArea}</textarea>
+                                                </p>
+                                                <div className="msg-client-info"
+                                                     ref={(node) => {
+                                                         if (node && appointment[0][0].hasCoAppointments && (parseInt(moment(currentTime).format("H")) >= 20)) {
+                                                             node.style.setProperty("top", '-325px', "important");
+                                                         }
+                                                     }}>
+                                                    {clients && clients.client && clients.client.map((client) => (
+                                                        client.clientId === appointment[0][0].clientId &&
+                                                        <div className="msg-inner">
+                                                            <p className="new-text">Запись</p>
+                                                            <p className="client-name-book">Клиент</p>
+                                                            <p className="name">{client.firstName} {client.lastName}</p>
+                                                            <p>{client.phone}</p>
+
+                                                            <p className="client-name-book">{appointmentServices.length > 1 ? 'Список услуг' : 'Услуга'}</p>
+                                                            {appointmentServices.map(service =>
+                                                                <p>{service}</p>)}
+                                                            <p>{moment(appointment[0][0].appointmentTimeMillis, 'x').format('HH:mm')} -
+                                                                {moment(appointment[0][0].appointmentTimeMillis, 'x').add(totalDuration, 'seconds').format('HH:mm')}</p>
+                                                            <p>{workingStaffElement.firstName} {workingStaffElement.lastName}</p>
+
+                                                            <a
+                                                                className="a-client-info"
+                                                                data-target=".client-detail"
+                                                                title="Просмотреть клиента"
+                                                                onClick={(e) => {
+                                                                    $('.client-detail').modal('show')
+                                                                    handleUpdateClient(client)
 
 
-                                resultMarkup = <AppointmentCell
-                                    appointment={appointment}
-                                    appointments={appointments}
-                                    workingStaffElement={workingStaffElement}
-                                    handleUpdateClient={handleUpdateClient}
-                                    approveAppointmentSetter={approveAppointmentSetter}
-                                    currentTime={currentTime}
-                                    key={key}
-                                    client={client}
-                                />
-
-
+                                                                }}>Просмотреть клиента</a>
+                                                        </div>))
+                                                    }
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                )
                             } else if ( reservedTime && reservedTime[0] && reservedTime[0].length > 0 ) {
-                                resultMarkup = <ReservedCell
-                                    reservedTime={reservedTime[0][0]}
-                                    staffId={workingStaffElement.staffId}
-                                    updateReservedId={updateReservedId}
-                                    />
+                                resultMarkup = (
+                                    <div className='reserve'>
+                                        <div className="notes color-grey"
+                                             style={{backgroundColor: "darkgrey"}}>
 
+                                            <p className="notes-title"
+                                               style={{cursor: 'default'}}>
+                                                                                <span className=""
+                                                                                      title="Онлайн-запись"/>
+                                                <span
+                                                    className="service_time"
+                                                >{moment(reservedTime[0][0].startTimeMillis, 'x').format('HH:mm')}
+                                                    -
+                                                    {moment(reservedTime[0][0].endTimeMillis, 'x').format('HH:mm')}</span>
 
+                                            </p>
+                                            <p className="notes-container"
+                                               style={{height: (parseInt(((moment.utc(reservedTime[0][0].endTimeMillis - reservedTime[0][0].startTimeMillis, 'x').format('x') / 60000 / 15) - 1) * 20)) + "px"}}>
+                                                                            <textarea
+                                                                                style={{color: '#5d5d5d'}}>{reservedTime[0][0].description}</textarea>
+                                                <span className="delete-notes"
+                                                      style={{right: '5px'}}
+                                                      data-toggle="modal"
+                                                      data-target=".delete-reserve-modal"
+                                                      title="Удалить"
+                                                      onClick={() => updateReservedId({
+                                                          reserveId: reservedTime[0][0].reservedTimeId,
+                                                          reserveStId: workingStaffElement.staffId
+                                                      })}
+                                                />
+                                            </p>
+                                        </div>
+                                    </div>
+                                )
                             } else {
-                                resultMarkup =(
-                                    <DefaultCell
-                                    currentTime={currentTime}
-                                    notExpired={notExpired}
-                                    workingStaffElement={workingStaffElement}
-                                    numbers={numbers}
-                                    time={time}
-                                    clDate={clDate}
-                                    changeTime={changeTime}
-                                    />
+                                resultMarkup = (
+                                    <div
+                                        id={currentTime <= moment().format("x") && currentTime >= moment().subtract(15, "minutes").format("x") ? 'present-time ' : ''}
+                                        className={`col-tab ${currentTime <= moment().format("x")
+                                        && currentTime >= moment().subtract(15, "minutes").format("x") ? 'present-time ' : ''}
+                                                                            ${currentTime < parseInt(moment().format("x")) ? '' : ""}
+                                                                            ${notExpired ? '' : "expired"}
+                                                                            ${clDate ? 'closedDateTick' : ""}`}
+                                        time={currentTime}
+                                        timeEnd={workingTimeEnd}
+                                        staff={workingStaffElement.staffId}
+                                        onClick={() => notExpired && changeTime(currentTime, workingStaffElement, numbers, false, null)}
+                                    ><span
+                                        className="fade-time">{moment(time, 'x').format("HH:mm")}</span>
+                                    </div>
                                 )
                             }
                             return resultMarkup;
