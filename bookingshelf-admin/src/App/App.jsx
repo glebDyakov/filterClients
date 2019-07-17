2bbd181d550fc3d29340d9bc1c1fbce451f01a12
vasylevych1998 <vasylevych1998@gmail.com>
2019-07-17 12:39:44 +0300

    fix autoReconnect socket

--- bookingshelf-admin/src/App/App.jsx
+++ bookingshelf-admin/src/App/App.jsx
@@ -180,10 +180,11 @@ class App extends React.Component {
         };
 
         socket.onmessage = function(event) {
+            debugger
             if (event.data[0]==='{'){
                 const finalData = JSON.parse(event.data);
                 if ((finalData.wsMessageType === "APPOINTMENT_CREATED") || (finalData.wsMessageType === "APPOINTMENT_DELETED")){
-                    this.handleSocketDispatch(finalData.payload);
+                    this.handleSocketDispatch(finalData);
                 }
             }
             console.log(`Сокет.пришли данные: ${event.data}`);
