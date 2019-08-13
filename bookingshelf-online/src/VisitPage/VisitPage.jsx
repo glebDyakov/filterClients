8739054ef34d7653015f4d06c8e8e49f4c040c04
vasylevych1998 <vasylevych1998@gmail.com>
2019-08-13 13:48:05 +0300

    notification

--- bookingshelf-online/src/VisitPage/VisitPage.jsx
+++ bookingshelf-online/src/VisitPage/VisitPage.jsx
@@ -7,6 +7,7 @@ import 'moment/locale/ru';
 import 'moment-timezone';
 import TabError from "../IndexPage/components/TabError";
 import TabCanceled from "../IndexPage/components/TabCanceled";
+import Header from "../IndexPage/components/Header";
 
 class VisitPage extends React.Component {
     constructor(props) {
@@ -76,23 +77,7 @@ class VisitPage extends React.Component {
 
             <div className="container_popups">
                 {isLoading && (<div className="loader"><img src={`${process.env.CONTEXT}public/img/spinner.gif`} alt=""/></div>)}
-                {info &&
-                <div className="modal_menu">
-                    <p className="firm_name">{info && info.companyName}</p>
-                    <div className="adress-phones">
-                        <p>{info && info["companyAddress" + info.defaultAddress]}</p>
-                    </div>
-                    <span className="mobile"></span>
-                    <p className="phones_firm">
-                        {info.companyPhone1 && <a href={"tel:" + info.companyPhone1}>{info.companyPhone1}</a>}
-                        {info.companyPhone2 && <a href={"tel:" + info.companyPhone2}>{info.companyPhone2}</a>}
-                        {info.companyPhone3 && <a href={"tel:" + info.companyPhone3}>{info.companyPhone3}</a>}
-                        <span className="closer"></span>
-                    </p>
-                    <div className="clear"></div>
-                </div>
-
-                }
+                {info && <Header info={info}/>}
                 {appointment && screen===1 && !isLoading &&
                     <div className="service_selection final-screen">
                         <div className="final-book">
