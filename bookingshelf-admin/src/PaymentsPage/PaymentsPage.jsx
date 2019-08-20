a79b6801f49b1d083c8b04a8dc33aa85cac7bbd5
vasylevych1998 <vasylevych1998@gmail.com>
2019-08-20 20:06:01 +0300

    changes

--- bookingshelf-admin/src/PaymentsPage/PaymentsPage.jsx
+++ bookingshelf-admin/src/PaymentsPage/PaymentsPage.jsx
@@ -286,7 +286,8 @@ class PaymentsPage extends Component {
     }
 
     render() {
-
+        const {authentication} = this.props;
+        debugger
         const {SMSCountChose, SMSCount, SMSPrice, chosenAct} = this.state;
         const {country, finalPrice, finalPriceMonth, chosenInvoice, invoiceSelected, list, defaultList, search, userSettings} = this.state;
         const {workersCount, period, specialWorkersCount} = this.state.rate;
@@ -665,9 +666,9 @@ class PaymentsPage extends Component {
                                                     </div>
                                                     <div className="customer-seller">
                                                         <div className="col-md-6 col-12 customer">
-                                                            <p>Покупатель: <strong>Online-zapis</strong></p>
-                                                            <p>Представитель: Андрей</p>
-                                                            <p>Адрес: Minsk 220089, Minsk Belarus</p>
+                                                            <p>Покупатель: <strong>{authentication.user.companyName}</strong></p>
+                                                            <p>Представитель: {authentication.user.profile.lastName} {authentication.user.profile.firstName}</p>
+                                                            <p>Адрес: {authentication.user.companyAddress1 || authentication.user.companyAddress2 || authentication.user.companyAddress3}</p>
                                                         </div>
 
                                                         <div className="col-md-6 col-12 seller">
@@ -970,9 +971,9 @@ class PaymentsPage extends Component {
 
 
 function mapStateToProps(state) {
-    const { company, payments} = state;
+    const { company, payments, authentication} = state;
     return {
-        company, payments
+        company, payments, authentication
     };
 }
 
