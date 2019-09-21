be68f71596db3d31dfa6ef2fa81f7b9578d59246
vasylevych1998 <vasylevych1998@gmail.com>
2019-09-21 14:55:07 +0300

    changes

--- bookingshelf-admin/src/_components/HeaderMain.jsx
+++ bookingshelf-admin/src/_components/HeaderMain.jsx
@@ -150,7 +150,11 @@ class HeaderMain extends React.PureComponent {
                     <span className="time_show" id="doc_time">{moment().format('HH:mm')}</span>
                     <div style={{ position: "relative" }}>
                         <span className="notification" onClick={() => {
+                            $('#__replain_widget').addClass('__replain_widget_show')
                             $('#__replain_widget_iframe').contents().find(".btn-img").click()
+                            $("#__replain_widget_iframe").contents().find(".hide-chat").bind("click", function() {
+                                $('#__replain_widget').removeClass('__replain_widget_show')
+                            });
                         }}/>
                     </div>
                     <a className="setting" onClick={this.openModal}/>
