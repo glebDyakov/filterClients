4fe6692e424e7c22df47c8981693a7ab46e818f2
vasylevych1998 <j%QfPmiA0764*vX%zka%>
2020-04-20 23:17:02 +0300

    changes

--- bookingshelf-admin/src/_components/PhoneInput.jsx
+++ bookingshelf-admin/src/_components/PhoneInput.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { isValidNumber } from 'libphonenumber-js';
 import ReactPhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 
@@ -9,76 +8,13 @@ class PhoneInput extends React.Component {
         super(props);
         this.state = {
             countryCode: 'by',
-            isValidPhone: this.validatePhone(props.value),
-            isTouchedPhone: false
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handleBlur = this.handleBlur.bind(this);
-        this.getIsValidPhone = this.getIsValidPhone.bind(this);
-        this.validatePhone = this.validatePhone.bind(this);
     }
-    
-    componentDidMount() {
-        this.getIsValidPhone(this.state.isValidPhone);
-        //this.updateCountryCode(this.props.countryCode)
-    }
-    
-    componentDidUpdate(prevProps, prevState) {
-        const { isValidPhone } = this.state;
-        if (prevState.isValidPhone !== isValidPhone) {
-            this.getIsValidPhone(isValidPhone)
-        }
-    }
-
-    // componentWillReceiveProps(newProps) {
-    //     if (newProps.countryCode && (newProps.countryCode !== this.props.countryCode)) {
-    //         this.updateCountryCode(newProps.countryCode)
-    //     }
-    // }
-
-    // updateCountryCode(newCountryCode) {
-    //     let countryCode;
-    //     switch (newCountryCode) {
-    //         case 'UKR':
-    //             countryCode = 'ua';
-    //             break;
-    //         case 'RUS':
-    //             countryCode = 'ru';
-    //             break;
-    //         default:
-    //             countryCode = 'by';
-    //     }
-    //     this.setState( { countryCode });
-    // }
 
     handleChange(phone) {
         const value = phone.startsWith('+') ? phone : `+${phone}`;
         this.props.handleChange(value.replace(/[() ]/g, ''));
-    }
-
-    handleBlur(event) {
-        const { value } = event.target;
-        const { isValidPhone, isTouchedPhone } = this.state;
-
-        const newState = {
-            isTouchedPhone: true
-        };
-        newState.isValidPhone = this.validatePhone(value);
-
-        if ((isValidPhone !== newState.isValidPhone) || (isTouchedPhone !== newState.isTouchedPhone)) {
-            this.setState(newState)
-        }
-    }
-
-    getIsValidPhone(isValidPhone) {
-        const { getIsValidPhone } = this.props
-        if (getIsValidPhone) {
-            getIsValidPhone(isValidPhone)
-        }
-    }
-
-    validatePhone(phone) {
-        return phone && phone.replace(/[+]/g, '') && isValidNumber(phone);
     }
 
     render() {
