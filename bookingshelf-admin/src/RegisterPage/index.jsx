cba3839a70a94d2001900a1c8992c0ff34f3632f
vasylevych1998 <j%QfPmiA0764*vX%zka%>
2020-04-22 12:15:26 +0300

    changes

--- bookingshelf-admin/src/RegisterPage/index.jsx
+++ bookingshelf-admin/src/RegisterPage/index.jsx
@@ -112,10 +112,11 @@ class Index extends React.Component {
 
         this.setState({ submitted: true });
         const { user, agreed, authentication, emailIsValid,  } = this.state;
-        user.phone = user.phone.startsWith('+') ? user.phone : `+${user.phone}`;
+        const body = JSON.parse(JSON.stringify(user));
+        body.phone = body.phone.startsWith('+') ? body.phone : `+${body.phone}`;
         const { dispatch } = this.props;
         if ( emailIsValid && user.companyName && user.email && user.password && user.timezoneId!=='' && user.countryCode!=='' && agreed && !authentication.registering) {
-            dispatch(userActions.register(user));
+            dispatch(userActions.register(body));
         }
     }
 
