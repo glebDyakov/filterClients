0576e234d72b06d68501b5c7fb559f30acfa7d4d
vasylevych1998 <j%QfPmiA0764*vX%zka%>
2020-07-13 15:24:58 +0300

    red line

--- bookingshelf-admin/src/CalendarPage/components/TabScrollLeftMenu.jsx
+++ bookingshelf-admin/src/CalendarPage/components/TabScrollLeftMenu.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const TabScrollLeftMenu = (props) => (
     <div className={`cell cell-height expired`}>
-        <div className={'cell hours' + (props.time.split(':')[1] === '00' ? '' : ' minutes')}>
+        <div className={'cell hours red-line-marker' + (props.time.split(':')[1] === '00' ? '' : ' minutes')}>
             <span>{props.time}</span>
         </div>
+        <div className="fon-for-red"></div>
     </div>
 );
 
