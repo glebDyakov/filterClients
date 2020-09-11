95cff067b0ec0becf69aa963df6953b814283932
VaDOs <tvadost2003@gmail.com>
2020-09-11 15:12:30 +0300

    fix price

--- bookingshelf-admin/src/CalendarPage/components/CellAppointment/CellAppointmentContent/CellAppointmentArea.jsx
+++ bookingshelf-admin/src/CalendarPage/components/CellAppointment/CellAppointmentContent/CellAppointmentArea.jsx
@@ -27,7 +27,7 @@ class CellAppointmentArea extends React.PureComponent {
   render() {
     const {
       isWeekBefore, appointment, textAreaId, minTextAreaHeight, maxTextAreaHeight,
-      resultTextAreaHeight, extraServiceText, services, movingVisit,
+      resultTextAreaHeight, extraServiceText, services, movingVisit, totalAmount,
     } = this.props;
 
     const serviceDetails = services && services.servicesList &&
@@ -59,7 +59,7 @@ class CellAppointmentArea extends React.PureComponent {
                   ? ('Клиент: ' + appointment.clientFirstName +
                   (appointment.clientLastName ? ` ${appointment.clientLastName}` : '')) + '\n'
                   : 'Без клиента'}</span>
-              <span style={{width: "43%"}} className="text-right client-name">{appointment.totalAmount} {appointment.currency}</span>
+              <span style={{width: "43%"}} className="text-right client-name">{totalAmount} {appointment.currency}</span>
             </span>
             <ul>
               <li className="service">{appointment.serviceName} {serviceDetails ? `(${serviceDetails})` : ''}</li>
