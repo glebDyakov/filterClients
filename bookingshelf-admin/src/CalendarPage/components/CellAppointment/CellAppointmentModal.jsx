1465ce2e63e794f3d881a3cad1d0a0d918f460d8
Vadym <tvadost2003@gmail.com>
2020-10-16 17:41:10 +0300

    choose visit status changes

--- bookingshelf-admin/src/CalendarPage/components/CellAppointment/CellAppointmentModal.jsx
+++ bookingshelf-admin/src/CalendarPage/components/CellAppointment/CellAppointmentModal.jsx
@@ -3,7 +3,7 @@ import moment from 'moment';
 import { appointmentActions, calendarActions } from '../../../_actions';
 import { access } from '../../../_helpers/access';
 import { connect } from 'react-redux';
-import {withTranslation} from "react-i18next";
+import { withTranslation } from 'react-i18next';
 
 const CellAppointmentModal = (props) => {
   const {
@@ -44,15 +44,19 @@ const CellAppointmentModal = (props) => {
     setOpenDropdown(false);
   };
 
+  const currentAppointmentStatus = getCurrentAppointmentStatus(appointment);
+  const currentAppointmentStatusString = getCurrentAppointmentStatusToString(currentAppointmentStatus);
+  const isStartedVisit = isVisitStarted(appointment.appointmentTimeMillis);
+
   return (!isStartMovingVisit &&
     <div onMouseDown={(e) => e.preventDefault()} className="cell msg-client-info">
       <div className={'cell msg-inner' + (appointment.clientId ? '' : ' pt-0')}>
         <p>
-          <p className="new-text">{t("Запись")}&nbsp;
+          <p className="new-text">{t('Запись')}&nbsp;
             <p className="visit-time">
               {moment(appointment.appointmentTimeMillis, 'x').format('HH:mm')}&nbsp;-&nbsp;{
-                moment(appointment.appointmentTimeMillis, 'x').add(totalDuration, 'seconds').format('HH:mm')
-              }
+              moment(appointment.appointmentTimeMillis, 'x').add(totalDuration, 'seconds').format('HH:mm')
+            }
             </p>
           </p>
           <button type="button" onClick={() => {
@@ -65,26 +69,19 @@ const CellAppointmentModal = (props) => {
           <p
             onClick={isOpenDropdownSelectClientStatus ? handleCloseSelectDropdown : handleOpenSelectDropdown}
             className={'dropdown-selected-item' + (isOpenDropdownSelectClientStatus ? ' opened' : '') +
-            (appointment.clientNotCome
-              ? ' clientNotCome'
-              : (appointment.clientConfirmed ? ' clientConfirmed' : ' default')
-            )}
+            (" " + currentAppointmentStatus)}
           >
-            {(appointment.clientNotCome
-              ? t('Клиент не пришел')
-              : (appointment.clientConfirmed ? t('Клиент подтвержден') : t('Ожидание клиента'))
-            )}
+            {t(currentAppointmentStatusString)}
           </p>
 
           {isOpenDropdownSelectClientStatus &&
           <div className="dropdown-selected">
-            {(appointment.clientNotCome || appointment.clientConfirmed) &&
+            {!isStartedVisit && appointment.clientConfirmed &&
             <p className="dropdown-item" onClick={() => {
               setSelectedItem(true);
               const params = currentAppointments.map((item) => {
                 return {
                   ...item,
-                  clientNotCome: false,
                   clientConfirmed: false,
                 };
               });
@@ -93,19 +90,17 @@ const CellAppointmentModal = (props) => {
               handleCloseSelectDropdown();
             }
             }>
-              {t("Ожидание клиента")}
+              {t('Ожидание клиента')}
             </p>
             }
 
 
-            {appointment.clientConfirmed === false &&
+            {!isStartedVisit && !appointment.clientConfirmed &&
             <p className="dropdown-item" onClick={() => {
               setSelectedItem('clientConfirm');
-              const newClientNotCome = false;
               const params = currentAppointments.map((item) => {
                 return {
                   ...item,
-                  clientNotCome: newClientNotCome,
                   clientConfirmed: true,
                 };
               });
@@ -113,10 +108,10 @@ const CellAppointmentModal = (props) => {
               handleCloseSelectDropdown();
             }
             }>
-              {t("Клиент подтвердил")}
+              {t('Клиент подтвердил')}
             </p>}
 
-            {appointment.clientNotCome !== true &&
+            {isStartedVisit && !appointment.clientNotCome &&
             <p className="dropdown-item" onClick={() => {
               setSelectedItem('clientNotCome');
               const newClientNotCome = true;
@@ -124,13 +119,28 @@ const CellAppointmentModal = (props) => {
                 return {
                   ...item,
                   clientNotCome: newClientNotCome,
-                  clientConfirmed: 'false',
                 };
               });
               dispatch(calendarActions.editAppointment2(JSON.stringify(params), currentAppointments[0].appointmentId));
               handleCloseSelectDropdown();
             }}>
-              {t("Клиент не пришел")}
+              {t('Клиент не пришел')}
+            </p>}
+
+            {isStartedVisit && appointment.clientNotCome &&
+            <p className="dropdown-item" onClick={() => {
+              setSelectedItem('clientNotCome');
+              const newClientNotCome = false;
+              const params = currentAppointments.map((item) => {
+                return {
+                  ...item,
+                  clientNotCome: newClientNotCome,
+                };
+              });
+              dispatch(calendarActions.editAppointment2(JSON.stringify(params), currentAppointments[0].appointmentId));
+              handleCloseSelectDropdown();
+            }}>
+              {t('Клиент пришел')}
             </p>}
           </div>
           }
@@ -140,51 +150,51 @@ const CellAppointmentModal = (props) => {
           style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
           className="client-name-book"
         >
-          <span className="client-title">{t("Клиент")}:</span>
+          <span className="client-title">{t('Клиент')}:</span>
         </p>
 
         {!appointment.clientId &&
-        <p className="name">{t("Без клиента")}</p>}
+        <p className="name">{t('Без клиента')}</p>}
 
         {appointment.clientId &&
-          <p
-            data-target=".client-detail"
-            onClick={(e) => {
-              if (access(4)) {
-                $('.client-detail').modal('show');
-                handleUpdateClient(appointment.clientId);
-              }
-            }}
-            className="name"
-          >
-            {appointment.clientFirstName
-              ? (appointment.clientFirstName + (appointment.clientLastName ? ` ${appointment.clientLastName}` : ''))
-              : t('Без клиента')
+        <p
+          data-target=".client-detail"
+          onClick={(e) => {
+            if (access(4)) {
+              $('.client-detail').modal('show');
+              handleUpdateClient(appointment.clientId);
             }
-          </p>
+          }}
+          className="name"
+        >
+          {appointment.clientFirstName
+            ? (appointment.clientFirstName + (appointment.clientLastName ? ` ${appointment.clientLastName}` : ''))
+            : t('Без клиента')
+          }
+        </p>
         }
         {access(12) && appointment.clientId && <p>{appointment.clientPhone}</p>}
         {companyTypeId === 2 && appointment.carBrand &&
-          <p>
-            <strong>Марка авто: </strong>
-            {appointment.carBrand}
-          </p>
+        <p>
+          <strong>Марка авто: </strong>
+          {appointment.carBrand}
+        </p>
         }
         {companyTypeId === 2 && appointment.carNumber &&
-          <p>
-            <strong>Гос. номер: </strong>
-            {appointment.carNumber}
-          </p>
+        <p>
+          <strong>Гос. номер: </strong>
+          {appointment.carNumber}
+        </p>
         }
 
         <div className="cash-choice">
           <div style={{ position: 'relative' }} className="check-box">
             <label>
-              {t("Безналичный расчет")}
+              {t('Безналичный расчет')}
               <input
                 className="form-check-input" type="checkbox"
                 checked={!appointment.cashPayment}
-                onChange={()=> {
+                onChange={() => {
                   const cashPayment = !currentAppointments[0].cashPayment;
                   const params = currentAppointments.map((item) => {
                     return {
@@ -204,11 +214,11 @@ const CellAppointmentModal = (props) => {
           </div>
           <div style={{ position: 'relative' }} className="check-box">
             <label>
-              {t("Наличный расчет")}
+              {t('Наличный расчет')}
               <input
                 className="form-check-input" type="checkbox"
                 checked={appointment.cashPayment}
-                onChange={()=> {
+                onChange={() => {
                   const cashPayment = !currentAppointments[0].cashPayment;
                   const params = currentAppointments.map((item) => {
                     return {
@@ -266,7 +276,8 @@ const CellAppointmentModal = (props) => {
 
         {appointment.clientId && <hr className="block-line"/>}
 
-        <p className="client-name-book">{appointmentServices.length > 1 ? t("Список услуг") + ':' : t("Услуга") + ':'}</p>
+        <p
+          className="client-name-book">{appointmentServices.length > 1 ? t('Список услуг') + ':' : t('Услуга') + ':'}</p>
         {appointmentServices.map((service, i) => {
           const details = services && services.servicesList &&
             (services.servicesList.find((service) => service.serviceId === appointment.serviceId) || {}).details;
@@ -275,18 +286,18 @@ const CellAppointmentModal = (props) => {
               {service.serviceName} {details ? `(${details})` : ''}
               <span style={{ display: 'inline-block', textAlign: 'left' }}>
                 {String(service.price) ? service.price : service.priceFrom} {service.currency} {
-                  !!service.discountPercent &&
-                    <span style={{
-                      display: 'inline',
-                      textAlign: 'left',
-                      color: 'rgb(212, 19, 22)',
-                    }}>
+                !!service.discountPercent &&
+                <span style={{
+                  display: 'inline',
+                  textAlign: 'left',
+                  color: 'rgb(212, 19, 22)',
+                }}>
                       ({service.totalAmount} {service.currency})
                     </span>
-                }
+              }
               </span>
               {!!service.discountPercent &&
-                <span style={{ textAlign: 'left', fontSize: '13px', color: 'rgb(212, 19, 22)' }}>
+              <span style={{ textAlign: 'left', fontSize: '13px', color: 'rgb(212, 19, 22)' }}>
                   {`${(service.discountPercent === (appointment && appointment.clientDiscountPercent))
                     ? t('Скидка клиента')
                     : t('Единоразовая скидка')
@@ -299,10 +310,11 @@ const CellAppointmentModal = (props) => {
         })
         }
 
-        <p className="all-price">{t("Итого")}:&nbsp;<span className="price">{totalAmount}&nbsp;{appointment && appointment.currency}</span></p>
+        <p className="all-price">{t('Итого')}:&nbsp;<span
+          className="price">{totalAmount}&nbsp;{appointment && appointment.currency}</span></p>
         <hr className="block-line"/>
 
-        <p className="staff-block-title">{t("Мастер")}</p>
+        <p className="staff-block-title">{t('Мастер')}</p>
         <div className="staff-block">
           <span className="img-container">
             <img
@@ -322,7 +334,7 @@ const CellAppointmentModal = (props) => {
         </div>
 
         {appointment.description &&
-        <p className="visit-note"><p className="bold-text">{t("Заметка")}:</p>&nbsp;{appointment.description}</p>}
+        <p className="visit-note"><p className="bold-text">{t('Заметка')}:</p>&nbsp;{appointment.description}</p>}
         {access(15) && isWeekBefore && (
           <div className="msg-inner-buttons">
             <div
@@ -331,7 +343,7 @@ const CellAppointmentModal = (props) => {
             >
               <div className="msg-inner-button-wrapper-1">
                 <img style={{ width: '20px' }} src={`${process.env.CONTEXT}public/img/appointment_move.svg`} alt=""/>
-                {t("Перенести")}
+                {t('Перенести')}
               </div>
               {/* <span className="move-white"/>*/}
             </div>
@@ -341,7 +353,7 @@ const CellAppointmentModal = (props) => {
             >
               <div className="msg-inner-button-wrapper-2">
                 <img style={{ width: '20px' }} src={`${process.env.CONTEXT}public/img/appointment_edit.svg`} alt=""/>
-                {t("Изменить")}
+                {t('Изменить')}
               </div>
               {/* <span className="move-white"/>*/}
             </div>
@@ -356,7 +368,7 @@ const CellAppointmentModal = (props) => {
             >
               <div className="msg-inner-button-wrapper-3">
                 <img style={{ width: '17px' }} src={`${process.env.CONTEXT}public/img/appointment_delete.svg`} alt=""/>
-                {t("Удалить")}
+                {t('Удалить')}
               </div>
               {/* <span className="cancel-white"/>*/}
             </div>
@@ -366,6 +378,34 @@ const CellAppointmentModal = (props) => {
     </div>
   );
 };
+
+function isVisitStarted(visitTime) {
+  return +moment().format('x') >= visitTime;
+}
+
+function getCurrentAppointmentStatus(appointment) {
+  if (isVisitStarted(appointment.appointmentTimeMillis)) {
+    if (!appointment.clientNotCome) return 'clientIsCome';
+    else if (appointment.clientNotCome) return 'clientNotCome';
+  } else {
+    if (!appointment.clientNotCome && !appointment.clientConfirmed) return 'waitClient';
+    else if (!appointment.clientNotCome && appointment.clientConfirmed) return 'clientConfirmed';
+  }
+}
+
+function getCurrentAppointmentStatusToString(status) {
+  console.log(status);
+  switch (status) {
+    case 'clientNotCome':
+      return "Клиент не пришел";
+    case 'clientIsCome':
+      return "Клиент пришел";
+    case 'waitClient':
+      return "Ожидание клиента";
+    case 'clientConfirmed':
+      return "Клиент подтвердил";
+  }
+}
 
 function mapStateToProps(state) {
   const {
@@ -381,4 +421,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(withTranslation("common")(React.memo(CellAppointmentModal)));
+export default connect(mapStateToProps)(withTranslation('common')(React.memo(CellAppointmentModal)));
