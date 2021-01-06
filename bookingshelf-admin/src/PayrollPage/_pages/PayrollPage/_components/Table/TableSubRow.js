364c2e7d80a96fc3b105e1a3cf1f2d83ab694035
Vadym <tvadost2003@gmail.com>
2021-01-06 18:14:21 +0200

    products

--- bookingshelf-admin/src/PayrollPage/_pages/PayrollPage/_components/Table/TableSubRow.js
+++ bookingshelf-admin/src/PayrollPage/_pages/PayrollPage/_components/Table/TableSubRow.js
@@ -15,31 +15,38 @@ class TableSubRow extends Component {
         <td>
           <h2 className="mobile-title desk-hidden">{t('Время услуги')}</h2>
           <p
-            className="list-text text-center">{moment('1900-01-01 00:00:00').add(payout.durationSec, 'seconds').format('H')}{t('ч')} {moment('1900-01-01 00:00:00').add(payout.durationSec, 'seconds').format('mm')} {t('минут')}</p>
+            className="list-text text-center">{payout.date ? '-' : (moment('1900-01-01 00:00:00').add(payout.durationSec, 'seconds').format('H') + ' ' + t('ч') + ' ' + moment('1900-01-01 00:00:00').add(payout.durationSec, 'seconds').format('mm') + ' ' + t('минут'))}</p>
         </td>
         <td>
           <h2 className="mobile-title desk-hidden">{t('Услуга')}</h2>
-          <p className="list-text">{payout.serviceName} <br/> {t('Цена')}: <span className="price">{payout.serviceCost.toFixed(2)} BYN</span></p>
+          <p className="list-text">{payout.date ? '-' : (<>{payout.serviceName} <br/> {t('Цена')}: <span
+            className="price">{payout.serviceCost.toFixed(2)} BYN</span></>)}</p>
         </td>
         <td>
           <h2 className="mobile-title desk-hidden">% {t('от услуги')}</h2>
-          <p className="list-text">{payout.servicePercent}%</p>
+          <p className="list-text">{payout.date ? '-' : payout.servicePercent + "%"}</p>
         </td>
         <td>
           <h2 className="mobile-title desk-hidden">{t('Товар')}</h2>
-          <p className="list-text">{payout.serviceProductPercent === 0 ? '-' : <> { payout.productName } <br/> {t('Цена')}: <span className="price">{payout.serviceProductCost} BYN </span> </>}</p>
+          <p className="list-text">
+            {payout.date
+              ? <>{payout.productName} <br/> {t('Цена')}: <span className="price">{payout.productCost} BYN </span></>
+              : <>{payout.serviceProductPercent === 0 ? '-' : <> {payout.productName}
+                <br/> {t('Цена')}: <span className="price">{payout.serviceProductCost} BYN </span> </>}</>}</p>
+
         </td>
         <td>
           <h2 className="mobile-title desk-hidden">% {t('от товара')}</h2>
-          <p className="list-text">{payout.serviceProductPercent === 0 ? '-' : payout.serviceProductPercent + '%'}</p>
+          <p
+            className="list-text">{payout.date ? <>{payout.productPercent + "%"}</> : <>{payout.serviceProductPercent === 0 ? '-' : payout.serviceProductPercent + '%'}</>}</p>
         </td>
         <td>
           <h2 className="mobile-title desk-hidden">{t('Доход Сотруд')}</h2>
-          <p className="list-text">{payout.staffRevenue.toFixed(2)} BYN</p>
+          <p className="list-text">{payout.date ? payout.staffProductRevenue.toFixed(2)  : payout.staffServiceRevenue.toFixed(2)} BYN</p>
         </td>
         <td>
           <h2 className="mobile-title desk-hidden">{t('Доход Компании')}</h2>
-          <p className="list-text">{payout.companyRevenue.toFixed(2)} BYN</p>
+          <p className="list-text">{payout.date ? payout.companyProductRevenue.toFixed(2)  : payout.companyServiceRevenue.toFixed(2)} BYN</p>
         </td>
 
       </tr>
