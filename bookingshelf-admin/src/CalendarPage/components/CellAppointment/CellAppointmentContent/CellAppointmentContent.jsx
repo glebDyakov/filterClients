56a5dcd0e64415d7250f70ecb52e1ae41f85c5c5
b.vasylevych <b.vasylevych@omisoft.net>
2021-02-03 10:58:37 +0200

    max textarea

--- bookingshelf-admin/src/CalendarPage/components/CellAppointment/CellAppointmentContent/CellAppointmentContent.jsx
+++ bookingshelf-admin/src/CalendarPage/components/CellAppointment/CellAppointmentContent/CellAppointmentContent.jsx
@@ -18,7 +18,7 @@ class CellAppointmentContent extends React.PureComponent {
     const {
       appointment,
       staff,
-      appointments: [],
+      appointments,
       timetable,
       reservedTime,
       workingStaffElement,
@@ -40,7 +40,7 @@ class CellAppointmentContent extends React.PureComponent {
       (totalDuration * 1000)) > i;
     const nearestAvailableMillis = getNearestAvailableTime(
       appointment.appointmentTimeMillis, appointment.appointmentTimeMillis, timetableItems,
-      appointments, reservedTime, staff, isOwnInterval,
+      [], reservedTime, staff, isOwnInterval,
     );
     const maxTextAreaCellCount = (nearestAvailableMillis - (appointment.appointmentTimeMillis + (step * 60000)))
       / 1000 / 60 / step;
