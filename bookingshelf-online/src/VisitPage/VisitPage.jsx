b47be622f1221698e0ae4fa04b79cbd26830c2a7
ilya shihutin <plague.plague@mail.ru>
2021-03-12 21:21:42 +0300

    fix: visitPage mob

--- bookingshelf-online/src/VisitPage/VisitPage.jsx
+++ bookingshelf-online/src/VisitPage/VisitPage.jsx
@@ -15,6 +15,8 @@ import { withTranslation } from "react-i18next";
 import MediaQuery from 'react-responsive'
 import Footer from "../IndexPage/components/Footer";
 import Loading from "../IndexPage/components/Loading";
+
+import Blob from '../IndexPage/components/Blob';
 class VisitPage extends React.Component {
     constructor(props) {
         super(props);
@@ -91,19 +93,16 @@ class VisitPage extends React.Component {
         const desctop = 600;
         const mob = 599;
         console.log(visitAppointments)
-        // const currentDay = moment(appointmentTimeMillis).format('DD MMMM YYYY,');
 
         const activeAppointment = visitAppointments && visitAppointments[0] && staff && staff.find(item => item.staffId === visitAppointments[0].staffId);
 
         let serviceInfo = null
         if (visitAppointments && visitAppointments[0]) {
-            let price = 0;
             let priceFrom = 0;
             let priceTo = 0;
             let duration = 0;
             let totalAmount = 0;
             visitAppointments.forEach((currentAppointment) => {
-                price += Number(currentAppointment.price)
                 priceFrom += Number(currentAppointment.priceFrom)
                 priceTo += Number(currentAppointment.priceTo)
                 duration += Number(currentAppointment.duration)
@@ -115,27 +114,6 @@ class VisitPage extends React.Component {
 
             serviceInfo = (
                 <React.Fragment>
-                    {/* <div style={{ display: 'inline-block' }} className="supperVisDet service_item">
-                    {(visitAppointments.length===1)?<p>{visitAppointments[0].serviceName}</p>:
-                        (<p>{t("Выбрано услуг")}: <strong className="service_item_price">{visitAppointments.length}</strong></p>)}
-                    <p><strong className="service_item_price">{priceFrom!==priceTo
-                        ? priceFrom+" - "+priceTo
-                        : price
-                    }</strong>&nbsp;<span>{visitAppointments[0].currency}</span></p>
-                    <span style={{ width: '100%' }} className="runtime">
-                        <strong>{moment.duration(parseInt(duration), "seconds").format(`h[ ${t("ч")}] m[ ${t("минут")}]`)}</strong>
-                        {visitAppointments && visitAppointments[0] && priceFrom===priceTo && !!visitAppointments[0].discountPercent && <span>({totalAmount} {visitAppointments[0].currency})</span>}
-                    </span>
-                    <div className="supperVisDet_info">
-                        <p className="supperVisDet_info_title">{t("Список услуг")}:</p>
-                        {visitAppointments.map(service => (
-                            <p>• {service.serviceName}</p>
-                        ))}
-                        <span className="supperVisDet_closer" />
-                    </div>
-                    <img className="tap-service-icon" src={`${process.env.CONTEXT}public/img/tap-service.svg`}/>
-                </div> */}
-
                     <div className="last_list_block">
                         <div className="last_list_caption">
                             <div className="last_list_img">
@@ -182,114 +160,165 @@ class VisitPage extends React.Component {
 
 
         return (
-
-            <div className="container_popups">
-                {isLoading && <Loading />}
-                {info && <Header info={info} />}
-                {!error && !deleted && appointment && screen === 1 && !isLoading &&
-                    <div className="service_selection final-screen">
-                        <div className="last_list">
-                            {appointment.serviceId && serviceInfo}
-                        </div>
-                        <div className="final-screen-block">
-                            <div className="title_block staff_title">
-
-                                <span className="prev_block"><span className="title_block_text"><a href={`/online/${this.props.match.params.company}`} onClick={() => {
-                                    this.props.dispatch(staffActions.toggleMovedVisitSuccess(false));
-                                }} className="title_block_text" >{t("Создать новую запись")}</a></span>
-                                </span>
-
-                            </div>
-                            <span><p>{t("Цены указаны на основе прайс-листа. Окончательная стоимость формируется на месте оказания услуги.")}</p></span>
-                            <div className=" cansel_block">
-                                {!(appointment && appointment.coStaffs && appointment.coStaffs.length > 0) &&
-                                    <button className="cansel-visit" onClick={() => {
-                                        this._move(visitAppointments)
-                                    }} >{t("Перенести визит")}{ARROW_ICON}</button>
-                                }
-                                <button className="cansel-visit" onClick={() => this.onCancelVisit()} >{t("Отменить визит")}{ARROW_ICON}</button>
-                            </div>
-                            {/* <div className="final-book">
-                                <p>{t("Ваша запись")}</p>
-                            </div> */}
-                            {/* <div className="specialist">
-                            {appointment.staffId &&
-                                <div>
-                                    <p className="img_container">
-                                        <img src={activeAppointment && activeAppointment.imageBase64 ? "data:image/png;base64," + activeAppointment.imageBase64 : `${process.env.CONTEXT}public/img/image.png`} alt="" />
-                                        <span>{appointment.staffName}</span>
-                                    </p>
-                                </div>
-                            }
-                            {appointment.serviceId && serviceInfo}
-                            {appointment.appointmentTimeMillis &&
-                                <div className="date_item_popup">
-                                    <strong>{moment(appointment.appointmentTimeMillis, 'x').format('DD MMMM YYYY')}</strong>
-                                </div>
-                            }
-                            {appointment.appointmentTimeMillis &&
-                                <div className="date_item_popup">
-                                    <strong>{moment(appointment.appointmentTimeMillis, 'x').format('HH:mm')}</strong>
-                                </div>
-                            }
-                        </div> */}
-                            {info && info.appointmentMessage && <p style={{
-                                color: 'red',
-                                textDecoration: 'underline',
-                                fontWeight: 'bold',
-                                textAlign: 'center',
-                                fontSize: '13px',
-                                marginBottom: '8px'
-                            }}>{info.appointmentMessage}</p>}
-
-
-                            {approveF && <div ref={(el) => { this.approvedButtons = el; }} className="approveF">
-                                <div className="modal_window_block">
-                                    <div className="modal_window_text">
-                                        <p className="modal_title">{t("Отменить визит")}?</p>
-                                        <img src={cansel} onClick={() => this.setterApproveF()} alt="cansel" />
+            <React.Fragment>
+                <MediaQuery maxWidth={mob}>
+                    <div className="container_popups">
+                   
+                        {isLoading && <Loading />}
+                        {info && <Header info={info} />}
+                        {!error && !deleted && appointment && screen === 1 && !isLoading &&
+                            <div className="service_selection final-screen">
+                                <div className="service_selection_block_six">
+                                    <div className="last_footer_block">
+                                        <a className="book_button_last" href={`/online/${this.props.match.params.company}`} onClick={() => {
+                                            this.props.dispatch(staffActions.toggleMovedVisitSuccess(false));
+                                        }}>{t("Создать новую запись")}</a><p>
+                                            {t("Нажимая кнопку записаться, вы соглашаетесь с условиями ")}&nbsp;
+              <a className="last_footer_block_a" href={`${origin}/user_agreement`} >{t("пользовательского соглашения")}</a>
+                                        </p>
                                     </div>
-                                    <div className="modal_window_btn">
-                                        <button className="approveFYes" onClick={() => {
-                                            deleted
-                                            if (appointment.customId) {
-                                                this.setScreen(2)
-                                                this._delete(appointment.customId)
+                                    <div className="last_list">
+                                        {appointment.serviceId && serviceInfo}
+                                    </div>
+                                    <div className="final-screen-block">
+                                        <span><p>{t("Цены указаны на основе прайс-листа. Окончательная стоимость формируется на месте оказания услуги.")}</p></span>
+                                        <div className=" cansel_block">
+                                            {!(appointment && appointment.coStaffs && appointment.coStaffs.length > 0) &&
+                                                <button className="cansel-visit" onClick={() => {
+                                                    this._move(visitAppointments)
+                                                }} >{t("Перенести визит")}{ARROW_ICON}</button>
                                             }
-                                        }}>{t("Да")}
-                                        </button>
-                                        <div style={{
-                                            height: "38px",
-                                            width: "1px",
-                                            backgroundColor: "rgba(9, 9, 58, 0.1)"
-                                        }}></div>
-                                        <button className="approveFNo" onClick={() => this.setterApproveF()}>{t("Нет")}
-                                        </button>
+                                            <button className="cansel-visit" onClick={() => this.onCancelVisit()} >{t("Отменить визит")}{ARROW_ICON}</button>
+                                        </div>
+                                        {info && info.appointmentMessage && <p className="final-book_hz">{t(`${info.appointmentMessage}`)}</p>}
+                                        {approveF && <div ref={(el) => { this.approvedButtons = el; }} className="approveF">
+                                            <div className="modal_window_block">
+                                                <div className="modal_window_text">
+                                                    <p className="modal_title">{t("Отменить визит")}?</p>
+                                                    <img src={cansel} onClick={() => this.setterApproveF()} alt="cansel" />
+                                                </div>
+                                                <div className="modal_window_btn">
+                                                    <button className="approveFYes" onClick={() => {
+                                                        deleted
+                                                        if (appointment.customId) {
+                                                            this.setScreen(2)
+                                                            this._delete(appointment.customId)
+                                                        }
+                                                    }}>{t("Да")}
+                                                    </button>
+                                                    <div style={{
+                                                        height: "38px",
+                                                        width: "1px",
+                                                        backgroundColor: "rgba(9, 9, 58, 0.1)"
+                                                    }}></div>
+                                                    <button className="approveFNo" onClick={() => this.setterApproveF()}>{t("Нет")}
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        }
                                     </div>
                                 </div>
                             </div>
-                            }
-                        </div>
-                    </div>
-                }
+                        }
 
-                {error &&
-                    <TabError
-                        error={error}
-                        setScreen={this.setScreen}
-                        companyId={this.props.match.params.company}
-                        isVisitPage
-                    />}
-                {deleted &&
-                    <TabCanceled
-                        setScreen={this.setScreen}
-                        companyId={this.props.match.params.company}
-                        isVisitPage
-                    />
-                }
-                <Footer />
-            </div>
+                        {error &&
+                            <TabError
+                                error={error}
+                                setScreen={this.setScreen}
+                                companyId={this.props.match.params.company}
+                                isVisitPage
+                            />}
+                        {deleted &&
+                            <TabCanceled
+                                setScreen={this.setScreen}
+                                companyId={this.props.match.params.company}
+                                isVisitPage
+                            />
+                        }
+                        <Footer />
+
+                    </div>
+                </MediaQuery>
+                <MediaQuery minWidth={desctop}>
+                <Blob screen={screen} />
+                    <div className="container_popups">
+                        {isLoading && <Loading />}
+                        {info && <Header info={info} />}
+                        {!error && !deleted && appointment && screen === 1 && !isLoading &&
+                            <div className="service_selection final-screen">
+                                <div className="last_list">
+                                    {appointment.serviceId && serviceInfo}
+                                </div>
+                                <div className="final-screen-block">
+                                    <div className="title_block staff_title">
+
+                                        <span className="prev_block"><span className="title_block_text"><a href={`/online/${this.props.match.params.company}`} onClick={() => {
+                                            this.props.dispatch(staffActions.toggleMovedVisitSuccess(false));
+                                        }} className="title_block_text" >{t("Создать новую запись")}</a></span>
+                                        </span>
+
+                                    </div>
+                                    <span><p>{t("Цены указаны на основе прайс-листа. Окончательная стоимость формируется на месте оказания услуги.")}</p></span>
+                                    <div className=" cansel_block">
+                                        {!(appointment && appointment.coStaffs && appointment.coStaffs.length > 0) &&
+                                            <button className="cansel-visit" onClick={() => {
+                                                this._move(visitAppointments)
+                                            }} >{t("Перенести визит")}{ARROW_ICON}</button>
+                                        }
+                                        <button className="cansel-visit" onClick={() => this.onCancelVisit()} >{t("Отменить визит")}{ARROW_ICON}</button>
+                                    </div>
+                                    {info && info.appointmentMessage && <p className="final-book_hz">{t(`${info.appointmentMessage}`)}</p>}
+                                    {approveF && <div ref={(el) => { this.approvedButtons = el; }} className="approveF">
+                                        <div className="modal_window_block">
+                                            <div className="modal_window_text">
+                                                <p className="modal_title">{t("Отменить визит")}?</p>
+                                                <img src={cansel} onClick={() => this.setterApproveF()} alt="cansel" />
+                                            </div>
+                                            <div className="modal_window_btn">
+                                                <button className="approveFYes" onClick={() => {
+                                                    deleted
+                                                    if (appointment.customId) {
+                                                        this.setScreen(2)
+                                                        this._delete(appointment.customId)
+                                                    }
+                                                }}>{t("Да")}
+                                                </button>
+                                                <div style={{
+                                                    height: "38px",
+                                                    width: "1px",
+                                                    backgroundColor: "rgba(9, 9, 58, 0.1)"
+                                                }}></div>
+                                                <button className="approveFNo" onClick={() => this.setterApproveF()}>{t("Нет")}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    }
+                                </div>
+                            </div>
+                        }
+
+                        {error &&
+                            <TabError
+                                error={error}
+                                setScreen={this.setScreen}
+                                companyId={this.props.match.params.company}
+                                isVisitPage
+                            />}
+                        {deleted &&
+                            <TabCanceled
+                                setScreen={this.setScreen}
+                                companyId={this.props.match.params.company}
+                                isVisitPage
+                            />
+                        }
+                        <Footer />
+                    </div>
+                </MediaQuery>
+            </React.Fragment>
         );
+
     }
 
     _delete(id) {
