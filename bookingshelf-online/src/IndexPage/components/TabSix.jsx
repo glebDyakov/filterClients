ad9a71f0d42e800a31b71547617a9ca4e68ef88d
Ilya Shihutin <plague.plague@mail.ru>
2021-03-15 12:18:47 +0300

    fix: delete y

--- bookingshelf-online/src/IndexPage/components/TabSix.jsx
+++ bookingshelf-online/src/IndexPage/components/TabSix.jsx
@@ -101,7 +101,7 @@ class TabSix extends PureComponent {
                                 }}>{t("Создать новую запись")}</a><p>
                                     {t("Нажимая кнопку записаться, вы соглашаетесь с условиями ")}&nbsp;
               <a className="last_footer_block_a"  href={`${origin}/user_agreement`} >{t("пользовательского соглашения")}</a>
-                                </p>y
+                                </p>
                             </div>
                             <div className="last_list">
                                 {serviceInfo}
