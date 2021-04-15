1cb8d9de1642100b7c0ff990d89d4cb4b64d8678
Mark Likhtar <mark-techart@bitbucket.org>
2021-04-15 15:38:30 +0300

    fix: widget subcompany city

--- bookingshelf-online/src/IndexPage/components/TabCompanySelection.jsx
+++ bookingshelf-online/src/IndexPage/components/TabCompanySelection.jsx
@@ -59,7 +59,7 @@ class TabCompanySelection extends PureComponent {
 
                                     <div>
                                         <div className="affiliate_name">{subcompany.companyName}</div>
-                                        <div className="stars desktop_visible" >{(info.city ? (info.city + ', ') : '') + subcompany[`companyAddress${subcompany.defaultAddress}`]}</div>
+                                        <div className="stars desktop_visible" >{(subcompany.city ? (subcompany.city + ', ') : '') + subcompany[`companyAddress${subcompany.defaultAddress}`]}</div>
                                     </div>
                                 </div>
 
